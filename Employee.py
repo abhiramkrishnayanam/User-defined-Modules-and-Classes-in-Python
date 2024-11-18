826d92a6436d823ff579674c63bb24f623b93f98
Abhiram K <abhiramkrishnayanam@gmail.com>
2024-11-18 16:47:17 +0530

    Add files via upload

---
+++ Employee.py
@@ -0,0 +1,8 @@
+class Employees:
+    def __init__(self,name,salary):
+        self.name_1=name
+        self.salary_1=salary
+    def get_name(self):
+        print(f"Name : {self.name_1}")
+    def get_salary(self):
+        print(f"Salary : {self.salary_1}")
